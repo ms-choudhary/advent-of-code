d7f3fb2c1de20b405fd9c6f198d7e594a7711c30
Mohit Choudhary <mohit.choudhary@scripbox.com>
2020-12-24 07:48:33 +0530

    day 16, part 2

--- 2020/16/main.go
+++ 2020/16/main.go
@@ -19,7 +19,7 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
-	ranges := [][]int{}
+	ranges := map[string][]int{}
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
@@ -28,7 +28,8 @@ func main() {
 		var r1low, r1high int
 		var r2low, r2high int
 
-		n, err := fmt.Sscanf(strings.Split(line, ":")[1][1:], "%d-%d or %d-%d", &r1low, &r1high, &r2low, &r2high)
+		fields := strings.Split(line, ":")
+		n, err := fmt.Sscanf(fields[1][1:], "%d-%d or %d-%d", &r1low, &r1high, &r2low, &r2high)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -36,8 +37,7 @@ func main() {
 			log.Fatal("couldn't parse complete line")
 		}
 
-		ranges = append(ranges, []int{r1low, r1high})
-		ranges = append(ranges, []int{r2low, r2high})
+		ranges[fields[0]] = []int{r1low, r1high, r2low, r2high}
 	}
 
 	myticket := []int{}
@@ -51,8 +51,6 @@ func main() {
 		myticket = parseTicket(line)
 	}
 
-	fmt.Println(myticket)
-
 	tickets := [][]int{}
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -62,23 +60,91 @@ func main() {
 		tickets = append(tickets, parseTicket(line))
 	}
 
-	invalidSum := 0
+	validTickets := [][]int{}
 	for _, t := range tickets {
-		for _, f := range t {
-			found := false
-			for _, r := range ranges {
-				if f >= r[0] && f <= r[1] {
-					found = true
+		if validTicket(t, ranges) {
+			validTickets = append(validTickets, t)
+		}
+	}
+
+	tempInd := make([]map[string]int, len(validTickets[0]))
+	for i := range tempInd {
+		tempInd[i] = map[string]int{}
+	}
+
+	for _, t := range validTickets {
+		for i, f := range t {
+			for k, r := range ranges {
+				if (f >= r[0] && f <= r[1]) || (f >= r[2] && f <= r[3]) {
+					tempInd[i][k]++
 				}
-			}
-			if !found {
-				fmt.Println(f)
-				invalidSum += f
 			}
 		}
 	}
 
-	fmt.Println(invalidSum)
+	index := make([]string, len(tempInd))
+
+	remInd := []int{}
+	for i := 0; i < len(tempInd); i++ {
+		remInd = append(remInd, i)
+	}
+
+	for {
+		newRemInd := []int{}
+		for _, i := range remInd {
+			interm := []string{}
+			for k, v := range tempInd[i] {
+				if v == len(validTickets) && !contains(index, k) {
+					interm = append(interm, k)
+				}
+			}
+
+			if len(interm) == 1 {
+				index[i] = interm[0]
+			} else {
+				newRemInd = append(newRemInd, i)
+			}
+		}
+		if len(newRemInd) == 0 {
+			break
+		}
+		remInd = newRemInd
+	}
+
+	fmt.Println(strings.Join(index, ","))
+
+	res := 1
+	for i, s := range index {
+		if strings.HasPrefix(s, "departure") {
+			res *= myticket[i]
+		}
+	}
+
+	fmt.Println(res)
+}
+
+func contains(arr []string, s string) bool {
+	for _, v := range arr {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func validTicket(t []int, ranges map[string][]int) bool {
+	for _, f := range t {
+		found := false
+		for _, r := range ranges {
+			if (f >= r[0] && f <= r[1]) || (f >= r[2] && f <= r[3]) {
+				found = true
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
 }
 
 func parseTicket(s string) []int {
